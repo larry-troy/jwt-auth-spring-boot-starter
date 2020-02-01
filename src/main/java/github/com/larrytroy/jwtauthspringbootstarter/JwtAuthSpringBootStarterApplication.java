513dbc9a9be3ecf20b2bff558354d43f860eec11
larry-troy <larrytroy91@gmail.com>
2020-02-01 15:02:04 +0600

    Init project

---
+++ src/main/java/github/com/larrytroy/jwtauthspringbootstarter/JwtAuthSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package github.com.larrytroy.jwtauthspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/com/larrytroy/jwtauthspringbootstarter/JwtAuthSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.larrytroy.jwtauthspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
