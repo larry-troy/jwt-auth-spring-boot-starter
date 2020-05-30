ef11c6ca339c0afd716f3b5767a50ebf78e0129e
larry-troy <larrytroy91@gmail.com>
2020-05-30 11:22:10 +0600

    Added Config for spring security

---
+++ src/main/java/github/com/larrytroy/jwtauthspringbootstarter/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package github.com.larrytroy.jwtauthspringbootstarter.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import github.com.larrytroy.jwtauthspringbootstarter.filter.JWTAuthenticationFilter;
+import github.com.larrytroy.jwtauthspringbootstarter.filter.JWTAuthorizationFilter;
+import github.com.larrytroy.jwtauthspringbootstarter.property.AuthProperty;
+import github.com.larrytroy.jwtauthspringbootstarter.service.JwtTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@ConditionalOnBean(name = "userDetailsServiceImpl")
+@ConditionalOnProperty("jwt-auth.auth.url")
+@RequiredArgsConstructor
+@EnableConfigurationProperties(AuthProperty.class)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenService jwtTokenService;
+
+    private final AuthProperty authProperty;
+
+    private final UserDetailsService userDetailsService;
+
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors().configurationSource(corsConfigurationSource)
+                .and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers(authProperty.getPermitUrls().toArray(new String[authProperty.getPermitUrls().size()]))
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager(),
+                        authProperty.getUrl(), jwtTokenService,
+                        new ObjectMapper()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtTokenService))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+}
